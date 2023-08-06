bd6b9176633ccf8e16b475381e86d51bc3a81b5a
unknown <louisbrent1992@gmail.com>
2023-08-05 18:14:01 -0700

    updated usermenu component

--- Frontend/src/Components/UserMenu.jsx
+++ Frontend/src/Components/UserMenu.jsx
@@ -1,37 +1,72 @@
-import { MenuItem } from "@mui/material";
+import React, { useCallback, useState } from "react";
+import { Badge, MenuItem } from "@mui/material";
+import { ShoppingCartOutlined } from "@mui/material/icons";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../redux/userRedux";
 import styled from "styled-components";
+import ProfilePic from "./ProfilePic";
 
 const Container = styled.div``;
 
-const LinkItem = styled(Link)`
+const NavLink = styled(Link)`
 	color: inherit;
+	text-shadow: ${(props) =>
+		props.shadow ? "2px 2px 2px rgba(0, 0, 0, 0.7)" : null};
+	font-weight: 500;
+	&:hover {
+		color: #e6b800;
+	}
 `;
 
 const SubMenu = styled.div`
 	position: absolute;
 	top: 0;
 	right: 0;
-	height: 55vh;
-	width: 30vw;
+	height: 57vh;
+	width: 38vw;
 	background: hsl(0 0% 100% /0.1);
 	backdrop-filter: blur(0.3rem);
 	filter: drop-shadow(2px 2px 2px rgba(0, 0, 0, 0.5));
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	justify-content: center;
-	z-index: -1;
+	justify-content: space-evenly;
 	color: ${(props) => props.color};
 	text-shadow: ${(props) => props.shadow && "2px 2px 2px rgba(0, 0, 0, 0.7)"};
 `;
 
-export default function UserMenu({ desktop, handleSubMenu, expandSub }) {
+const UserContainer = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	gap: 1rem;
+`;
+
+const Cart = styled(ShoppingCartOutlined)`
+	color: ${(props) => props.color};
+	&:hover {
+		color: #e6b800;
+	}
+`;
+
+export default function UserMenu({
+	desktop,
+	LinkColor,
+	LinkShadow,
+	linkStyle,
+	cartStyle,
+	quantity,
+}) {
+	const [expandSub, setExpandSub] = useState(false);
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
+
 	const currentUser = useSelector((state) => state.user.currentUser);
+
+	const toggleSubMenu = useCallback(() => {
+		setExpandSub(!expandSub);
+	}, [expandSub]);
 
 	const handleLogout = () => {
 		dispatch(logout());
@@ -40,45 +75,74 @@ export default function UserMenu({ desktop, handleSubMenu, expandSub }) {
 
 	return (
 		<Container>
-			{desktop ? (
-				expandSub && (
+			{desktop &&
+				(expandSub ? (
 					<>
-						<SubMenu onClick={handleSubMenu}>
-							<MenuItem>
-								<LinkItem to={`/${currentUser?.username}/account_overview`}>
-									Account
-								</LinkItem>
-							</MenuItem>
-							<MenuItem>
-								<LinkItem to="/cart/wishlist">Wishlist</LinkItem>
-							</MenuItem>
-							<MenuItem>
-								<LinkItem to="/support">Support</LinkItem>
-							</MenuItem>
+						<SubMenu onClick={toggleSubMenu}>
+							<UserContainer>
+								<NavLink
+									color={LinkColor}
+									shadow={LinkShadow}
+									style={linkStyle}
+									onClick={toggleSubMenu}
+								>
+									<ProfilePic src={currentUser?.avatar} alt="Profile Avatar" />
+									user logged in, {currentUser?.username}
+								</NavLink>
+								<NavLink to="/cart">
+									<Badge
+										badgeContent={quantity}
+										color="primary"
+										overlap="rectangular"
+									>
+										<Cart
+											style={cartStyle}
+											htmlColor={LinkColor}
+											shadow={LinkShadow}
+										/>
+									</Badge>
+								</NavLink>
+							</UserContainer>
+
+							<NavLink to={`/${currentUser?.username}/account_overview`}>
+								Account
+							</NavLink>
+
+							<NavLink to="/cart/wishlist">Wishlist</NavLink>
+
+							<NavLink to="/support">Support</NavLink>
+
 							<MenuItem onClick={handleLogout} style={{ color: "red" }}>
 								Logout
 							</MenuItem>
 						</SubMenu>
 					</>
-				)
-			) : (
-				<>
-					<MenuItem>
-						<LinkItem to={`/${currentUser?.username}/account_overview`}>
-							Account
-						</LinkItem>
-					</MenuItem>
-					<MenuItem>
-						<LinkItem to="/cart/wishlist">Wishlist</LinkItem>
-					</MenuItem>
-					<MenuItem>
-						<LinkItem to="/support">Support</LinkItem>
-					</MenuItem>
-					<MenuItem onClick={handleLogout} style={{ color: "red" }}>
-						Logout
-					</MenuItem>
-				</>
-			)}
+				) : (
+					<UserContainer>
+						<NavLink
+							color={LinkColor}
+							shadow={LinkShadow}
+							onClick={toggleSubMenu}
+							style={linkStyle}
+						>
+							<ProfilePic src={currentUser?.avatar} alt="Profile Avatar" />
+							user logged in, {currentUser?.username}
+						</NavLink>
+						<NavLink to="/cart">
+							<Badge
+								badgeContent={quantity}
+								color="primary"
+								overlap="rectangular"
+							>
+								<Cart
+									style={cartStyle}
+									htmlColor={LinkColor}
+									shadow={LinkShadow}
+								/>
+							</Badge>
+						</NavLink>
+					</UserContainer>
+				))}
 		</Container>
 	);
 }
